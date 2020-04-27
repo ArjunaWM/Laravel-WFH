fbb38d7b5c52ddcb04b60187ead442ac1a242b95
Arjuna Wira Mahardika <arjuna_wira_27rpl@student.smktelkom-mlg.sch.id>
2020-04-27 20:46:01 +0700

    Ketiga bagian frontend

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@index')->name('admin');
